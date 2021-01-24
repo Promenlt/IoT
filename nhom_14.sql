a15fbe280ae2170a44264801321f5ffe94319323
Promenlt <68494848+Promenlt@users.noreply.github.com>
2021-01-24 16:32:06 +0700

    +Add db

---
+++ nhom_14.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th1 24, 2021 lúc 10:31 AM
+-- Phiên bản máy phục vụ: 10.4.14-MariaDB
+-- Phiên bản PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `nhom_14`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `pump`
+--
+
+CREATE TABLE `pump` (
+  `Pump_ID` int(255) NOT NULL,
+  `Pump_Humd` int(255) DEFAULT NULL,
+  `Pump_Status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `pump`
+--
+
+INSERT INTO `pump` (`Pump_ID`, `Pump_Humd`, `Pump_Status`) VALUES
+(1, 62, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `User_ID` int(255) NOT NULL,
+  `User_Name` varchar(255) NOT NULL,
+  `User_Password` varchar(255) NOT NULL,
+  `User_Phone` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`User_ID`, `User_Name`, `User_Password`, `User_Phone`) VALUES
+(1, 'nhom14', 'nhom14', 333394599);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `pump`
+--
+ALTER TABLE `pump`
+  ADD PRIMARY KEY (`Pump_ID`);
+
+--
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`User_ID`,`User_Name`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
